f3b4e3770917cc81a53f85cdc92f59bfbcab0004
Alex Hort-Francis <ajhf@hotmail.co.uk>
2025-04-13 18:27:28 +0100

    Initial commit - Add Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div 
+        className="flex justify-center items-center h-screen text-3xl font-bold"
+      >
+        utc-cube-poc-01
+      </div>
+    </>
+  )
+}
+
+export default App
